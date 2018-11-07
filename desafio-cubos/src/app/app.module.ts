91f857076f81a7864b91f8ae37b8b6af0bf8898d
Jose Roberto Leite da Silva Filho <jose.leite@SLT-002200.solutis.net.br>
2018-11-07 09:52:00 -0300

    Ajustes e tela de erro

--- desafio-cubos/src/app/app.module.ts
+++ desafio-cubos/src/app/app.module.ts
@@ -1,20 +1,29 @@
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilmDetailsComponent } from './film-details/film-details.component';
 import { ItemFilmeComponent } from './item-filme/item-filme.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes =[
+  {path: ':id', component: FilmDetailsComponent},
+  {path: '**', component: PagenotfoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FilmDetailsComponent,
-    ItemFilmeComponent
+    ItemFilmeComponent, 
+    PagenotfoundComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- desafio-cubos/src/app/service/movieDBService.ts
+++ desafio-cubos/src/app/service/movieDBService.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {Jsonp} from '@angular/http';
-import 'rxjs/Rx';
 
 
 @Injectable()
